9f426d335051be5088f33f81fb83aaf799f925d9
hugo2710BR <hugosoares2710@outlook.pt>
2025-03-02 19:08:11 +0000

    restore usestate branch

--- reactacademyapp/src/Calculator.js
+++ reactacademyapp/src/Calculator.js
@@ -1,69 +1,70 @@
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
-const btn4 = document.getElementById("btn4");
-const btn5 = document.getElementById("btn5");
-const btn6 = document.getElementById("btn6");
-const btn7 = document.getElementById("btn7");
-const btn8 = document.getElementById("btn8");
-const btn9 = document.getElementById("btn9");
-const btn0 = document.getElementById("btn0");
-const btnPlus = document.getElementById("btnPlus");
-const btnDivide = document.getElementById("btnDivide");
-const btnTimes = document.getElementById("btnTimes");
-const btn1Clean = document.getElementById("btnClean");
-const btnEquals = document.getElementById("btnEquals");
-
-const result = document.querySelector(".Result");
-
-// btn1.addEventListener("click", () => {
-//   const btn1Value = btn1.textContent;
-//   console.log(btn1Value);
-//   result.innerHTML = btn1Value;
-// });
-// btn2.addEventListener("click", () => {
-//   const btn2Value = btn2.textContent;
-//   console.log(btn2Value);
-//   result.innerHTML = btn2Value;
-// });
-// btn3.addEventListener("click", () => {
-//   const btn3Value = btn3.textContent;
-//   console.log(btn3Value);
-//   result.innerHTML = btn3Value;
-// });
-// btn4.addEventListener("click", () => {
-//   const btn4Value = btn4.textContent;
-//   console.log(btn4Value);
-//   result.innerHTML = btn4Value;
-// });
-// btn5.addEventListener("click", () => {
-//   const btn5Value = btn5.textContent;
-//   console.log(btn5Value);
-//   result.innerHTML = btn5Value;
-// });
+import React, { useState } from "react";
 
 function Calculator() {
+  const [result, setResult] = useState("0");
+
+  const btnLogic = (btnContent) => {
+    if (result === "0") {
+      setResult(btnContent);
+    } else {
+      setResult(result + btnContent);
+    }
+  };
+
+  const btnClean = () => {
+    document.querySelector(".Result").innerHTML = "0";
+  };
+
   return (
     <div className="Container">
-      <div className="Result">0</div>
+      <div className="Result">{result}</div>
       <div className="ButtonContainer">
-        <button id="btn1">1</button>
-        <button id="btn2">2</button>
-        <button id="btn3">3</button>
-        <button id="btnPlus">+</button>
+        <button id="btn1" onClick={() => btnLogic("1")}>
+          1
+        </button>
+        <button id="btn2" onClick={() => btnLogic("2")}>
+          2
+        </button>
+        <button id="btn3" onClick={() => btnLogic("3")}>
+          3
+        </button>
+        <button id="btnPlus" onClick={() => btnLogic("+")}>
+          +
+        </button>
         <br></br>
-        <button id="btn4">4</button>
-        <button id="btn5">5</button>
-        <button id="btn6">6</button>
-        <button id="btnDivision">/</button>
+        <button id="btn4" onClick={() => btnLogic("4")}>
+          4
+        </button>
+        <button id="btn5" onClick={() => btnLogic("5")}>
+          5
+        </button>
+        <button id="btn6" onClick={() => btnLogic("6")}>
+          6
+        </button>
+        <button id="btnDivision" onClick={() => btnLogic("/")}>
+          /
+        </button>
         <br></br>
-        <button id="btn7">7</button>
-        <button id="btn8">8</button>
-        <button id="btn9">9</button>
-        <button id="btnTimes">*</button>
+        <button id="btn7" onClick={() => btnLogic("7")}>
+          7
+        </button>
+        <button id="btn8" onClick={() => btnLogic("8")}>
+          8
+        </button>
+        <button id="btn9" onClick={() => btnLogic("9")}>
+          9
+        </button>
+        <button id="btnTimes" onClick={() => btnLogic("x")}>
+          X
+        </button>
         <br></br>
-        <button id="btn0">0</button>
-        <button id="btnClean">C</button>
+        <button id="btn0" onClick={() => btnLogic("0")}>
+          0
+        </button>
+        <button id="btnClean" onClick={btnClean}>
+          C
+        </button>
+        <button id="btnEquals">=</button>
       </div>
     </div>
   );
